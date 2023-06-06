9a0ab74446bc7014faf73318510f001607118e86
Thắng Lê <lequocthang307@gmail.com>
2023-06-06 16:34:34 +0700

    thực hành 1

---
+++ 07_bien_kieu_du_lieu_va_toan_tu/practice/khai_bao_va_su_dung_bien.js
@@ -0,0 +1,18 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+let width = 20
+let height = 10
+let area = width * height
+
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+document.write('Area = ' + area)
