e5d1aaa91fdc2d87d7fcb68fceb500c6450e84f2
Thắng Lê <lequocthang307@gmail.com>
2023-06-06 11:06:01 +0700

    bài thực hành 6

---
+++ 06_javascript/practice/js_nhung.js
@@ -0,0 +1,12 @@
+function showmessage1(){
+    alert("xin chồ")
+}
+function showmessage2(){
+    document.getElementById("messdiv").innerHTML = "hế lô !!!"
+}
+function showmessage3(){
+    document.write("chào miết")
+}
+function showmessage4(){
+    console.log("bye")
+}
